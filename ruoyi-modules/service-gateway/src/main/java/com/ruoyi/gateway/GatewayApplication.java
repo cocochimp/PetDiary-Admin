3607ac7bbdec5690aab833835488add4c60b6960
lvwenji <lvwenji@kuaishou.com>
2023-12-24 10:53:55 +0800

    项目重构：service-system和service-gateway初步重构

---
+++ ruoyi-modules/service-gateway/src/main/java/com/ruoyi/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.ruoyi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 网关启动程序
+ * 
+ * @author cocochimp
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GatewayApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依网关启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+}
