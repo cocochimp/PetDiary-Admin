53bb6f2a159ef53d43cf5d8fe38c713ffbe26da8
lvwenji <lvwenji@kuaishou.com>
2023-12-24 13:50:49 +0800

    项目重构：service-auth初步重构

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/constant/ServiceNameConstants.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/constant/ServiceNameConstants.java
@@ -10,7 +10,7 @@ public class ServiceNameConstants
     /**
      * 认证服务的serviceid
      */
-    public static final String AUTH_SERVICE = "ruoyi-auth";
+    public static final String AUTH_SERVICE = "service-auth";
 
     /**
      * 系统模块的serviceid
